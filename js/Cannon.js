6b828d29672855a4cc19417ecded17a200754361
OmTiwari1 <86567204+OmTiwari1@users.noreply.github.com>
2021-08-02 11:54:18 -0600

    Add files via upload

---
+++ js/Cannon.js
@@ -0,0 +1,18 @@
+class Cannon{
+  constructor(x,y,w,h,angle) {
+this.x=x
+this.y=y
+this.w=w
+this.h=h
+this.angle=angle
+}
+display(){
+    push()
+    translate(this.x,this.y)
+    rotate(this.angle)
+    rect (-10,-20,this.w,this.h)
+    pop ()
+    arc(this.x-30,this.y+90,140,200,PI,TWO_PI)
+}
+
+}
